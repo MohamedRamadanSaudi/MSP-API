d3b6b67a7099a85e45007e1e5ae3a546f45439f7
Mohamed Ramadan <91095064+MohamedRamadanSaudi@users.noreply.github.com>
2025-01-28 17:12:19 +0200

    refactor: modify app.js, remove www file and remove debug, http-errors and jade packages

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 /* Packages */
-var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
@@ -47,15 +46,10 @@ var app = express();
 app.use(cors());
 app.use(helmet());
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "uploads")));
 app.use(rateLimiter);
 
@@ -76,20 +70,14 @@ app.use("/teamHistoryClient", teamHistoryClientRouter);
 app.use("/blogsClient", blogsClientRouter);
 app.use("/galleryClient", galleryClientRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+
+// Catch-all route handler for undefined routes
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+var PORT = process.env.PORT || '3000';
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
-
-module.exports = app;
